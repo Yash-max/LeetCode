47ef10c7d2604ea4833fcb073da158e455c81cc8
Yash Khandelwal <53614677+Yash-max@users.noreply.github.com>
2023-03-29 12:46:54 +0530

    Time: 3 ms (20.37%), Space: 41.8 MB (22.62%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,15 +1,8 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         int n = days.length;
-        int[] dp = new int[366];
         
         Set<Integer> set = new HashSet<>();
-        
-        // for(int d = 1; d <= 365; d++) {
-        //     dp[d] = costs[0] * d;
-        //     if(d >= 7) dp[d] = Math.min(dp[d], costs[1] + dp[d-7]);
-        //     if(d >= 30) dp[d] = Math.min(dp[d], costs[2] + dp[d-30]);
-        // }
         
         int maxDay = 0;
         
@@ -18,9 +11,11 @@ class Solution {
             maxDay = Math.max(maxDay, d);
         }
         
+        int[] dp = new int[maxDay+1];
+        
         int minCost = Math.min(costs[0], Math.min(costs[1], costs[2]));
         
-        for(int i = 1; i <= 365; i++) {
+        for(int i = 1; i <= maxDay; i++) {
             if(i > maxDay) break;
             if(!set.contains(i)) {
                 dp[i] = dp[i-1];
@@ -43,10 +38,6 @@ class Solution {
             }
         }
         
-        return dp[days[n-1]];
-        /*
-            [1,4,6,7,8,20]
-            [7,2,15]
-        */
+        return dp[maxDay];
     }
 }
